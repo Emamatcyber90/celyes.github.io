b455e3b61e6303cc01e83c88bad0977b05588632
celyes <celyes02@gmail.com>
2019-01-05 22:49:08 +0100

    modify index.html file

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,7 +2,7 @@ var app = new Vue({
     el: "#app",
     data: {
         title: "Circale",
-        name : prompt("ما هو إسمك ؟"),
+        name : "",
         cfb: "https://fb.com/celyes01",
         gfb: "https://fb.com/groups/CircaleWebTeam/"
     }
