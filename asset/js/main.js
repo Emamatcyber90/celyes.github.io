fc6aaa48f3e7380ee2c34bcb8efc9e40ac867a09
celyes <celyes02@gmail.com>
2019-01-05 22:24:03 +0100

    add several templates for practice

---
+++ asset/js/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        title: "Circale",
+        name : prompt("ما هو إسمك ؟"),
+        cfb: "https://fb.com/celyes01",
+        gfb: "https://fb.com/groups/CircaleWebTeam/"
+    }
+})
