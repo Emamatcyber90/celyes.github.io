2482c88838f6882d9b083897a9985fba41fa2e78
celyes <celyes02@gmail.com>
2019-01-05 22:31:17 +0100

    add function checkName

--- asset/js/main.js
+++ asset/js/main.js
@@ -5,5 +5,15 @@ var app = new Vue({
         name : prompt("ما هو إسمك ؟"),
         cfb: "https://fb.com/celyes01",
         gfb: "https://fb.com/groups/CircaleWebTeam/"
+    },
+    methods: {
+        checkName: function(){
+            if(this.name==""){
+                document.getElementById('#welcome').style.display="none"
+            }
+        }
+    },
+    beforeMount(){
+        this.checkName()
     }
 })
