aabd747c08b3257a1d27d5436bc753edf306c9e3
celyes <celyes02@gmail.com>
2019-01-05 22:55:25 +0100

    fix broken links

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,8 +2,6 @@ var app = new Vue({
     el: "#app",
     data: {
         title: "Circale",
-        name : "",
-        cfb: "https://fb.com/celyes01",
-        gfb: "https://fb.com/groups/CircaleWebTeam/"
+        name : ""
     }
 });
